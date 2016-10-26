04ae7f8d0c24b3461f0c15435b11699f5f8b2a08
Cristian Rodriguez <cristianrodriguez@Cristians-MacBook-Pro.local>
2016-10-26 12:00:35 -0700

    game completed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,71 +2,71 @@ var q1 = {
 	question:'Who was the 2002 champion?',
 	options: ['Argentina','Brazil','Germany','Italy'],
 	rightAnswer: 'Brazil',
-	image:'<img src="https://media.giphy.com/media/AkgFjTCtqyLyo/giphy.gif" id="image">',
+	image:'<img src="assets/images/brazilGermany.gif" id="image">',
 	legend: '<div id="legend">Brazil Won the tournament, defeating Germany 2:0</div>'
 };
 var q2 = {
 	question:"Who scored Spain's winning goal in the 2010 final",
 	options: ['David Villa','Fernando Torres','Sergio Ramos','Andres Iniesta'],
 	rightAnswer: 'Andres Iniesta',
-	image:'<img src="https://media.giphy.com/media/2TbHEBc4z8qnm/giphy.gif" id="image">',
-	legend:'<div id="legend">Andres Iniesta scored the winning goal on minute 103 of over time </div>'
+	image:'<img src="assets/images/Iniesta.gif" id="image">',
+	legend:'<div id="legend">Andres Iniesta scored the winning goal on minute 116 of over time </div>'
 };
 var q3 = {
 	question:'What team did Germany beat in the 2014 final?',
 	options: ['Argentina','Colombia','France','Spain'],
 	rightAnswer: 'Argentina',
-	image:'<img src="http://33.media.tumblr.com/e7a7a0b4582c994fcd471073627089d8/tumblr_n8pj4eaip51tu2oiko1_400.gif" id="image">',
+	image:'<img src="assets/images/gotzeGoal.gif" id="image">',
 	legend:'<div id="legend">Germany defeated Messi\'s Argentina 1:0 with Mario Gotze\'s late goal</div>'
 };
 var q4 = {
 	question:'What team has been in every single World Cup?',
 	options: ['Uruguay','England','Brazil','Germany'],
 	rightAnswer: 'Brazil',
-	image:'<img src="https://media.giphy.com/media/csDBjMwrQfnlC/giphy.gif" id="image">',
+	image:'<img src="assets/images/brazilDancing.gif" id="image">',
 	legend:'<div id="legend">Brazil has participated in every one of the 20 World Cup finals.</div>'
 };
 var q5 = {
 	question:'What country hosted the 1990 FIFA World Cup?',
 	options: ['Argentina','Brazil','Germany','Italy'],
 	rightAnswer: 'Italy',
-	image:'<img src="https://theburningbloggerofbedlam.files.wordpress.com/2014/06/great-italia90a.jpg" id="image">',
+	image:'<img src="assets/images/italia90.jpg" id="image">',
 	legend:'<div id="legend">Italy was the host, Salvatore Schillaci was the top scorer of the tournament with 6 goals.</div>'
 };
 var q6 = {
 	question:'Where was the first World Cup held?',
 	options: ['England','Uruguay','Germany','Italy'],
 	rightAnswer: 'Uruguay',
-	image:'<img src="http://www.soccergamblers.com/images/1930-uruguay-world-cup.jpg" id="image">',
+	image:'<img src="assets/images/uruguay1930.jpg" id="image">',
 	legend:'<div id="legend">Uruguay was the host for the first World Cup. It was played in the year 1930.</div>'
 };
 var q7 = {
 	question:'What player was sent off for headbutting an oponent in the 2006 final?',
 	options: ['Zinedine Zidane','Andrea Pirlo','Thierry Henry','Marco Materazzi'],
 	rightAnswer: 'Zinedine Zidane',
-	image:'<img src="https://media.giphy.com/media/nHFRIyStg60X6/giphy.gif" id="image">',
+	image:'<img src="assets/images/zidaneHeadbutt.gif" id="image">',
 	legend:'<div id="legend">Zinedine Zidane was sent off in his last-ever match, for headbutting Italy\'s Marco Materazzi\'s chest.</div>'
 };
 var q8 = {
 	question:'Who is the all time top scorer?',
 	options: ['Lionel Messi','Ronaldo Nazario de Lima','Pele','Miroslav Klose'],
 	rightAnswer: 'Miroslav Klose',
-	image:'<img src="https://media.giphy.com/media/l7IRjqleM7eXS/giphy.gif" id="image">',
+	image:'<img src="assets/images/klose.gif" id="image">',
 	legend:'<div id="legend">Germany\'s Miroslav Klose went on to score a record 16 goals across 4 consecutive tournaments between 2002 and 2014.</div>'
 };
 var q9 = {
 	question:'What teams played the final in Mexico 1986',
 	options: ['Brazil and West Germany','Argentina and West Germany','Italy and Brazil','Argentina and Brazil'],
 	rightAnswer: 'Argentina and West Germany',
-	image:'<img src="http://i.telegraph.co.uk/multimedia/archive/02972/maradona_1986_2972173b.jpg" id="image">',
+	image:'<img src="assets/images/argentina.jpg" id="image">',
 	legend:'<div id="legend">Argentina beat West Germany on the 1986 final</div>'
 };
 var q10 = {
 	question:'Where will the 2018 World Cup be played?',
 	options: ['Qatar','Germany','China','Russia'],
 	rightAnswer: 'Russia',
-	image:'<img src="http://img.fifa.com/mm/photo/tournament/mascot/02/84/55/65/2845565_xbig-lnd.jpg" id="image">',
-	legend:'<div id="legend">Andres Iniesta scored the winning goal on minute 103 of over time </div>'
+	image:'<img src="assets/images/russia2018.jpg" id="image">',
+	legend:'<div id="legend">The 2018 World Cup in Russia will have a total of 13 sites, 16 stadiums, of which thirteen will be brand new and other three which will be completely refurbished. </div>'
 };
 
 var questions = [q1,q2,q3,q4,q5,q6,q7,q8,q9,q10];
@@ -74,23 +74,23 @@ var currentQuestion = 0;
 var userGuess = '';
 var correctAnswers = 0;
 var wrongAnswers = 0;
+var unanswered = 0;
 var userChoise ='';
 var correctAnswer='';
-var timeLeft = 10;
+var timeLeft = 20;
+var counter = 0;
+
 
 $('.startButton').click(function() {
-	startGame();
+	displayNext();
 });
 
 //this is the function that starts the game
-function startGame() {
-	//removes the start button 
-	$('.startButton').fadeOut(1000);
 
-	displayNext();
-}
 
 function answerSelection() {
+	timeLeft = 20;
+	counter = setInterval(timer,1000)
 	$('.answerChoices').click(function () {
 		userGuess = $(this).data('userGuess');
 		rightAnswer = questions[currentQuestion].rightAnswer;
@@ -98,30 +98,52 @@ function answerSelection() {
 			$('.gameBox').html('Correct!').addClass('question');
 			$('.gameBox').append(questions[currentQuestion].legend).addClass('question');
 			$('.gameBox').append(questions[currentQuestion].image);
+			resetTimer();
 			correctAnswers++;
 			currentQuestion++;
-			setTimeout(displayNext, (6500));
+			setTimeout(displayNext,6500);
 		} else {
 			$('.gameBox').html('Wrong!').addClass('question');
 			$('.gameBox').append(questions[currentQuestion].legend).addClass('question');
 			$('.gameBox').append(questions[currentQuestion].image);
+			resetTimer();
 			wrongAnswers++;
 			currentQuestion++;
-			setTimeout(displayNext, (6500));
+			setTimeout(displayNext,6500);
 		}
 	});
 }
 
 function displayNext () {
+	// resetTimer();
 	if (currentQuestion >= questions.length) {
 		$('.gameBox').html('<div class="question">Test Completed</div>');
 		$('.gameBox').append('<div id="correctIncorrect">Correct Answers: '+correctAnswers+'</div>');
 		$('.gameBox').append('<div id="correctIncorrect">Wrong Answers: '+wrongAnswers+'</div>')
+		$('.gameBox').append('<div id="correctIncorrect">Unanswered Questions: '+unanswered+'</div>')
+		$('.gameBox').append('<button class="restartButton hvr-radial-in">Restart Game?</button>')
+			$('.restartButton').click(function () {
+			$('.gameBox').empty();
+
+			currentQuestion = 0;
+			userGuess = '';
+			correctAnswers = 0;
+			wrongAnswers = 0;
+			unanswered = 0;
+			userChoise ='';
+			correctAnswer='';
+			timeLeft = 20;
+			counter = 0;
+
+			$('.gameBox').html("<button class='startButton btn btn-primary center-block'>Start</button>"); 
+			$('.startButton').click(function() {
+				displayNext();
+			});
+		});
 		return false;
 	}
-	resetTimer();
-	var counter = setInterval(timer,1000);
-	$('.gameBox').html('<div class="timeLeft">Time Left: <span id="time">10</span></div>');
+	// counter = setInterval(timer,1000);
+	$('.gameBox').html('<div class="timeLeft">Time Left: <span id="time">20</span></div>');
 	var questionToAnswer = $('<div>').append(questions[currentQuestion].question).addClass('question');
 	$('.gameBox').append(questionToAnswer);
 
@@ -139,20 +161,20 @@ function displayNext () {
 
 
 function timer() {
-	if (timeLeft<1) {
-		$('.gameBox').html('You Run Out of Time!').addClass('question');
+	if (timeLeft === 1) {
+		$('.gameBox').html('<div class="question"><b>Your time is up!</b></div>');
 		$('.gameBox').append(questions[currentQuestion].legend).addClass('question');
 		$('.gameBox').append(questions[currentQuestion].image);
-		wrongAnswers++;
+		resetTimer();
+		unanswered++;
 		currentQuestion++;
-		setTimeout(displayNext, 6500)
-		return false;
+		setTimeout(displayNext,6500);
 	}
 	timeLeft--;
 	$('#time').html(timeLeft);
 }
 
 function resetTimer () {
-	timeLeft = 10;
-	clearInterval(timer);
+	clearInterval(counter);
+	counter = 0;
 }
